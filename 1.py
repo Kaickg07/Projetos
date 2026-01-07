8b3280d307375dc285fe0b5ebfb119c2d0791e3c
Kaick <gomeskaick54@gmail.com>
2026-01-07 09:23:01 -0300

    Gerador de senhas Python

---
+++ 1.py
@@ -0,0 +1,11 @@
+import random
+
+caracter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+entrada = int(input("Digite a número de caracteres que sua senha deve ter: "))
+x = []
+for i in range(0,entrada):
+    imp = random.choice(caracter)
+    x.append(imp)
+
+print(f"Senha: {"".join(x)}")
+
